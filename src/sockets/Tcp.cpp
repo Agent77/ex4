5f4f38029507af27043f28c1ae5cd757e28e233e
Tiki <lobel.tiki@gmail.com>
2016-12-26 18:41:39 +0200

    intial commit of cpps

---
+++ src/sockets/Tcp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tiki Lobel on 12/26/16.
+//
+
+#include "Tcp.h"
---
+++ src/sockets/Tcp.h
@@ -0,0 +1,14 @@
+//
+// Created by Tiki Lobel on 12/26/16.
+//
+
+#ifndef PRACTICE6_TCP_H
+#define PRACTICE6_TCP_H
+
+
+class Tcp {
+
+};
+
+
+#endif //PRACTICE6_TCP_H
